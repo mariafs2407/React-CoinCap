662e385bb7652bedad02c925c00ee276da8d05b1
MariaF <mariafs2407@gmail.com>
2023-02-02 19:47:39 -0500

    alineando

--- src/components/cripto/CriptoPage.jsx
+++ src/components/cripto/CriptoPage.jsx
@@ -10,18 +10,18 @@ const CriptoPage = () => {
     // nos devuelve los parametros que existen en la url
     const params = useParams();
 
-    const [cripto,cargandoCripto] = usePetition(`assets/${params.id}`)
-    const [history,cargandoHistory] = usePetition(`assets/${params.id}/history?interval=d1`)
+    const [cripto, cargandoCripto] = usePetition(`assets/${params.id}`)
+    const [history, cargandoHistory] = usePetition(`assets/${params.id}/history?interval=d1`)
 
-    if(cargandoCripto || cargandoHistory) return <span>cargando...</span>
+    if (cargandoCripto || cargandoHistory) return <span>cargando...</span>
     return (
         <div className='cripto-page-container'>
-            { 
-                (cripto) && <CriptoInfo cripto ={cripto}/>}
+            {
+                (cripto) && <CriptoInfo cripto={cripto} />}
 
             <div className='history'>
                 {
-                    (history) && <CriptoHistorial history={history}/>}
+                    (history) && <CriptoHistorial history={history} />}
 
             </div>
         </div>
