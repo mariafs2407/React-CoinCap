e248fb673ddb3cd1dfbea63a1ee288d9872e2ebf
MariaF <mariafs2407@gmail.com>
2023-02-02 21:05:05 -0500

    arreglando fecha español

--- src/components/cripto/info/CriptoHistorial.jsx
+++ src/components/cripto/info/CriptoHistorial.jsx
@@ -15,7 +15,7 @@ const CriptoHistorial = ({ history }) => {
                     history.map(({ time, date, priceUsd }) => (
                         <tr key={time}>
                             {/* <td className='label'>{dayjs(date).format('DD/MM/YYYY')}</td> */}
-                            <td>{new Date(date).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" })}</td>
+                            <td>{new Date(date).toLocaleDateString('es', { weekday: "long", year: "numeric", month: "short", day: "numeric" })}</td>
                             <td className='price'>{parseFloat(priceUsd).toFixed(2)}</td>
                         </tr>
 
